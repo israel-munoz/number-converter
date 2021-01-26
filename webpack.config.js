1d0cc741d1b810f598165244c50f7e47b50d182d
Israel Muñoz Valdez <israel.munoz.v@gmail.com>
2021-01-26 09:00:02 -0700

    Upgraded to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,60 @@
-const path = require('path');
+const { resolve, join } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devtool: isProduction ? null : 'inline-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 1000,
-    watchContentBase: true,
-    headers: {
-      'Cache-Control': 'max-age=31536000'
-    }
-  },
-  resolve: {
-    extensions: ['.js']
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: [
-        'babel-loader',
-        'eslint-loader'
-      ]
-    }, {
-      test: /\.css$/,
-      use: [{
-        loader: MiniCssExtractPlugin.loader,
-        options: {
-          publicPath: 'css/',
-          minimize: true
-        }
-      }, {
-        loader: 'css-loader'
-      }],
-      exclude: /node_modules/
-    }, {
-      test: /\.(svg|png)$/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'images/',
-          emitFile: true
-        }
-      }],
-      exclude: /node_modules/
-    }]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Numbers Converter',
-      filename: 'index.html',
-      template: 'src/index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles.css',
-      chunkFilename: '[id].css'
-    })
-  ],
-  optimization: {
-    minimizer: [
-      new UglifyJsWebpackPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: !isProduction
-      }),
-      new OptimizeCssPlugin()
+    entry: {
+        build: './src/main.js'
+    },
+    output: {
+        filename: '[name].js?v=[hash]',
+        path: resolve(__dirname, 'dist')
+    },
+    devtool: isProduction ? null : 'inline-source-map',
+    devServer: {
+        contentBase: join(__dirname, 'dist'),
+        compress: true,
+        port: 1000,
+        watchContentBase: true
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            ]
+        }, {
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader'
+            ],
+            exclude: /node_modules/
+        }, {
+            test: /\.(svg|png)$/,
+            type: 'asset/resource',
+            generator: {
+                filename: 'images/[name][ext]'
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Numbers Converter',
+            filename: 'index.html',
+            template: 'src/index.html',
+            chunks: ['build']
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+            chunkFilename: '[name].css?v=[hash]'
+        })
     ]
-  }
-};
+}
