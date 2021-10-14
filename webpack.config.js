d1d7d8769cb82a8e5d9b62694f0c18297bd47a5d
Israel Muñoz <israel.munoz.v@gmail.com>
2021-10-14 09:45:49 -0700

    v1.1.2 Upgraded packages to fix security vulnerabilities.

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,60 @@
-const { resolve, join } = require('path');
+const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    entry: {
-        build: './src/main.js'
-    },
-    output: {
-        filename: '[name].js?v=[hash]',
-        path: resolve(__dirname, 'dist')
-    },
-    devtool: isProduction ? null : 'inline-source-map',
-    devServer: {
-        contentBase: join(__dirname, 'dist'),
-        compress: true,
-        port: 1000,
-        watchContentBase: true
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: [
-                {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            ]
-        }, {
-            test: /\.css$/,
-            use: [
-                MiniCssExtractPlugin.loader,
-                'css-loader'
-            ],
-            exclude: /node_modules/
-        }, {
-            test: /\.(svg|png)$/,
-            type: 'asset/resource',
-            generator: {
-                filename: 'images/[name][ext]'
-            }
-        }]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Numbers Converter',
-            filename: 'index.html',
-            template: 'src/index.html',
-            chunks: ['build']
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'styles.css',
-            chunkFilename: '[name].css?v=[hash]'
-        }),
-        new ESLintPlugin()
-    ]
-}
+  entry: {
+    build: './src/main.js'
+  },
+  output: {
+    filename: '[name].js?v=[hash]',
+    path: resolve(__dirname, 'dist')
+  },
+  devtool: isProduction ? null : 'inline-source-map',
+  devServer: {
+    compress: true,
+    port: 1000
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      ]
+    }, {
+      test: /\.css$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader'
+      ],
+      exclude: /node_modules/
+    }, {
+      test: /\.(svg|png)$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'images/[name][ext]'
+      }
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Numbers Converter',
+      filename: 'index.html',
+      template: 'src/index.html',
+      chunks: ['build']
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+      chunkFilename: '[name].css?v=[hash]'
+    }),
+    new ESLintPlugin()
+  ]
+};
